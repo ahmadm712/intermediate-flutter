8e53781da68a2202d80bbcc028b3f296599947bc
ahmadm712 <ahmadmuji2808@gmail.com>
2023-02-19 23:38:26 +0700

    done first nav 2.0

---
+++ lib/routes/router_delegates.dart
@@ -0,0 +1,59 @@
+import 'package:dicoding_intermediate/model/quote.dart';
+import 'package:dicoding_intermediate/screen/quote_detail_page.dart';
+import 'package:dicoding_intermediate/screen/quote_detail_screen.dart';
+import 'package:dicoding_intermediate/screen/quotes_list_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyRouteDelegate extends RouterDelegate
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin {
+  final GlobalKey<NavigatorState> _navigatorKey;
+  MyRouteDelegate() : _navigatorKey = GlobalKey<NavigatorState>();
+  String? selectedQoete;
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: _navigatorKey,
+      pages: [
+        MaterialPage(
+          child: QuotesListScreen(
+            quotes: quotes,
+            onTapped: (quoteId) {
+              selectedQoete = quoteId;
+              notifyListeners();
+            },
+          ),
+          key: const ValueKey(
+            'QuotesListPage',
+          ),
+        ),
+        if (selectedQoete != null)
+          QuoteDetailsPage(
+            key: const ValueKey('QuoteDetailsPage'),
+            child: QuoteDetailsScreen(
+              quoteId: selectedQoete!,
+            ),
+          )
+      ],
+      onPopPage: (route, result) {
+        final didPop = route.didPop(result);
+        if (!didPop) {
+          return false;
+        }
+
+        selectedQoete = null;
+        notifyListeners();
+
+        return true;
+      },
+    );
+  }
+
+  @override
+  GlobalKey<NavigatorState>? get navigatorKey => _navigatorKey;
+
+  @override
+  Future<void> setNewRoutePath(configuration) {
+    throw UnimplementedError();
+  }
+}
